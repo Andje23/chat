af31a8989c0f35a289e0cbd7ed28fc72946c0900
Andje23 <andje23@gmail.com>
2022-09-14 22:24:05 +0300

    create method run in class MessageMonitor

--- client/client.py
+++ client/client.py
@@ -18,3 +18,12 @@ class MessageMonitor(QtCore.QThread):
         self.server_socket: socket.socket = server_socket
         self.private_key: int = private_key
         self.message: str = None
+
+    def run(self) -> None:
+        while True:
+            try: # Data from the interlocutor (encrypted) Данные от собеседника (зашифрованные)
+                self.message = self.server_socket.recv(1024)
+                decrypt_message = rsa.decrypt(self.message, self.private_key)
+                self.my_signal.emit(decrypt_message.decode('utf-8'))
+            except:  # Data from the server (not encrypted) Данные от сервера (не зашрованные)
+                self.my_signal.emit(self.message.decode('utf-8'))
